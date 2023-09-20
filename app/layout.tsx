e2085cc0630ea3d53cced53225de837d9ccc9c6f
Daniel Salahi <dan.salahii@gmail.com>
2023-09-20 11:13:05 +0300

    initialize next 13.5 app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+// wrap the project with Theme component
+import { Theme } from "@radix-ui/themes";
+
+import "@radix-ui/themes/styles.css";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +20,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <body className={inter.className}>
+
+      <Theme accentColor="blue" grayColor="olive" scaling="105%">
+      {children}
+      </Theme>
+
+      </body>
     </html>
   )
 }
